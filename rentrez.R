81c5cf4be0df01a0308c63a8e6d30eb5d1f3ef71
Frank <fbearoff@users.noreply.github.com>
2021-08-24 15:02:02 -0400

    construct PMID search query function

--- rentrez.R
+++ rentrez.R
@@ -1,2 +1,14 @@
 library(rentrez)
-r_search  <-  entrez_search(db="pubmed", term="Parkinson"
+
+#100 most recent entries of term plus top N genes by padj
+retrieve_PMIDs <- function(term_name, N){
+    search_result <<- list()
+    for (gene in z[order(padj)][1:N, gene_symbol]){
+             r_search <- entrez_search(db="pubmed", term=paste0(term_name,"[TIAB] AND ", gene, "[TIAB]"),retmax=100)
+             search_result <<- append(search_result, setNames(list(as.character(r_search$ids)), gene))
+             # search_count <<- rbind(search_count, gene = r_search$count)
+    }
+    return(search_result)
+}
+
+r_fetch <- entrez_fetch(db="pubmed", id = r_search$ids, rettype="abstract")
